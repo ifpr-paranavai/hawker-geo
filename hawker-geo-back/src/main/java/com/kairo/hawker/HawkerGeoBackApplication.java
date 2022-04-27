aaaabfa6141cfa72297ae4fa8db5519dacb13e72
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-04-26 22:23:42 -0300

    First commit back-end (excluding data source)

---
+++ hawker-geo-back/src/main/java/com/kairo/hawker/HawkerGeoBackApplication.java
@@ -0,0 +1,14 @@
+package com.kairo.hawker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class HawkerGeoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HawkerGeoBackApplication.class, args);
+    }
+
+}
