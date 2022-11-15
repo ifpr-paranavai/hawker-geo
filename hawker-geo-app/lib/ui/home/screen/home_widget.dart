ed257c081a637da971cdde84eb45684db9408e53
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-11-15 16:22:32 -0300

    Initial develop of Accept Call Pop Up & Confirm Call Creation Pop Up

--- hawker-geo-app/lib/ui/home/screen/home_widget.dart
+++ hawker-geo-app/lib/ui/home/screen/home_widget.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:hawker_geo/core/model/hawker_category_enum.dart';
@@ -107,10 +108,8 @@ class HomeWidget extends State<HomePage> {
 
   Widget _getFloatingButton(BuildContext context) {
     var locationButton = FloatingActionButton(
-      onPressed: () {
-        setState(() {
-          mapController.move(userLocation!, 17);
-        });
+      onPressed: () async {
+        await _ratingPopUp(context);
       },
       backgroundColor: kPrimaryLightColor.withOpacity(0.8),
       child: Icon(Icons.my_location_sharp, color: Colors.red.withOpacity(0.8)),
@@ -387,5 +386,295 @@ class HomeWidget extends State<HomePage> {
         );
       },
     );
+  }
+
+  _acceptPopUp(BuildContext context) async {
+    // TODO - componentizar
+    await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                decoration:
+                    BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10)),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Recebendo chamado de:",
+                        style: boldTitle.copyWith(fontSize: 25),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(
+                              radius: 30,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(
+                                          "https://www.masslive.com/resizer/kNl3qvErgJ3B0Cu-WSBWFYc1B8Q=/arc-anglerfish-arc2-prod-advancelocal/public/W5HI6Y4DINDTNP76R6CLA5IWRU.jpeg")),
+                                  borderRadius: BorderRadius.all(Radius.circular(1000)),
+                                  color: kPrimaryLightColor,
+                                ),
+                              )),
+                          const SizedBox(
+                            width: 10,
+                          ),
+
+                          const Text("Usuário Cliente 001",
+                              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+                          // IconButton(
+                          //     onPressed: callButtonOnPressed,
+                          //     icon: const GradientIcon(
+                          //       icon: Icons.campaign,
+                          //       rectSize: 26,
+                          //       iconSize: 30,
+                          //     ))
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            "Avaliação geral:",
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                          ),
+                          RatingBar.builder(
+                            initialRating: 4,
+                            ignoreGestures: true,
+                            minRating: 1,
+                            itemSize: 25,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                            itemBuilder: (context, _) => const Icon(
+                              Icons.star,
+                              color: kFourthLightColor,
+                            ),
+                            onRatingUpdate: (rating) => 0,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Material(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(50),
+                        child: Row(
+                          // mainAxisSize: MainAxisSize.min,
+                          // crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: kPrimaryLightColor,
+                                      borderRadius: BorderRadius.circular(50)),
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.done,
+                                      color: Colors.white,
+                                    ),
+                                    iconSize: 40,
+                                    splashRadius: 50,
+                                  ),
+                                ),
+                                Text(
+                                  "Aceitar",
+                                  style: boldTitle.copyWith(fontSize: 15),
+                                )
+                              ],
+                            ),
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: kSecondDarkColor,
+                                      borderRadius: BorderRadius.circular(50)),
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.close,
+                                      color: Colors.white,
+                                    ),
+                                    iconSize: 40,
+                                    splashRadius: 50,
+                                  ),
+                                ),
+                                Text(
+                                  "Negar",
+                                  style: boldTitle.copyWith(fontSize: 15),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ));
+  }
+
+  _ratingPopUp(BuildContext context) async {
+    // TODO - componentizar
+    await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                decoration:
+                    BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10)),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Nos conte como foi o atendimento!",
+                        textAlign: TextAlign.center,
+                        style: boldTitle.copyWith(fontSize: 20),
+                      ),
+                      const Divider(color: Colors.black, height: 20, endIndent: 20, thickness: 1),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(
+                              radius: 30,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(
+                                          "https://www.masslive.com/resizer/kNl3qvErgJ3B0Cu-WSBWFYc1B8Q=/arc-anglerfish-arc2-prod-advancelocal/public/W5HI6Y4DINDTNP76R6CLA5IWRU.jpeg")),
+                                  borderRadius: BorderRadius.all(Radius.circular(1000)),
+                                  color: kPrimaryLightColor,
+                                ),
+                              )),
+                          const SizedBox(
+                            width: 10,
+                          ),
+
+                          const Text("Usuário Cliente 001",
+                              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+                          // IconButton(
+                          //     onPressed: callButtonOnPressed,
+                          //     icon: const GradientIcon(
+                          //       icon: Icons.campaign,
+                          //       rectSize: 26,
+                          //       iconSize: 30,
+                          //     ))
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            "Nota:",
+                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                          ),
+                          RatingBar.builder(
+                            initialRating: 0,
+                            minRating: 1,
+                            itemSize: 35,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                            itemBuilder: (context, _) => const Icon(
+                              Icons.star,
+                              color: kFourthLightColor,
+                            ),
+                            onRatingUpdate: (rating) => 0,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      const Text(
+                        "Descrição:",
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                      ),
+                      Material(
+                        color: Colors.transparent,
+                        child: Container(
+                          color: Colors.grey.withOpacity(0.2),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              maxLines: 4, //or null
+                              decoration: InputDecoration.collapsed(
+                                  hintText: "Comente sobre o atendimento...",
+                                  // fillColor: Colors.grey.withOpacity(0.3),
+                                  ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            decoration: BoxDecoration(
+                                color: Colors.grey.withOpacity(0.3),
+                                borderRadius: BorderRadius.circular(50)),
+                            child: const Text(
+                              "Atendimento rápido",
+                              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            decoration: BoxDecoration(
+                                color: Colors.grey.withOpacity(0.3),
+                                borderRadius: BorderRadius.circular(50)),
+                            child: const Text(
+                              "Boa conversa",
+                              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ));
   }
 }
